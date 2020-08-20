87835328a220c2e1359a21fc5a39f72f6ed6f75f
csysl <925821946@qq.com>
2020-08-20 21:47:13 +0800

    'done'

--- main.cpp
+++ main.cpp
@@ -12,17 +12,22 @@ struct TreeNode {
 
 class Solution {
 public:
-    bool IsBalanced_Solution(TreeNode* pRoot) {
-
+    bool IsBalanced_Solution(TreeNode *pRoot) {
+        auto a = length(pRoot->left);
+        auto b = length(pRoot->right);
+        if (abs(a - b) > 1) return false;
+        auto ba = IsBalanced_Solution(pRoot->left);
+        auto bb = IsBalanced_Solution(pRoot->right);
+        return ba && bb;
     }
 
-    int length(TreeNode *root){
-        if (root==nullptr)return 0;
+    int length(TreeNode *root) {
+        if (root == nullptr)return 0;
 
-        auto a=length(root->left);
-        auto b=length(root->right);
+        auto a = length(root->left);
+        auto b = length(root->right);
 
-        if(abs(a-b)>1)
+        return max(a, b) + 1;
     }
 };
 
